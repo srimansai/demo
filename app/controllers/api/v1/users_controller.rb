988c3cc1e79e2f8bb9cef42aedb732ff752a1b17
sriman <sriman6907@gmail.com>
2023-01-02 13:21:22 +0530

    demo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,28 +1,27 @@
 class Api::V1::UsersController < ApplicationController
-  
+  before_action :set_controller, only: [:show, :update, :destroy]
+
   def index
-    render json: User.all.page(params[:page])
+    @user = User.all.page(params[:page])
+    render json: @user
   end  
 
   def show
-    render json: User.find_by(params[:id])
+    render json: @user
   end
-  
+
   def create
     @user = User.new(user_params)
 
-    if @user
-      @user.save
+    if @user.save
      render json: @user, status: :created
     else
      render json: @user.errors, status: :unprocessable_entity
     end 
   end
 
-  def update 
-    @user = User.find(params[:id])
-    if @user
-      @user.update(user_params)
+  def update
+    if @user.update(user_params)
       render json: @user, status: 200
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -30,11 +29,16 @@ class Api::V1::UsersController < ApplicationController
   end    
 
   def destroy
-    User.find(params[:id]).destroy!
-    head :no_content
+    @user.destroy!
+    render json: @user
   end
 
    private
+
+   def set_controller
+    @user = User.find(params[:id])
+  end
+
 
   def user_params
     params.require(:user).permit(
